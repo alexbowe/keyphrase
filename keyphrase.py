6925f0b9d284392b98163ccc9bee3345a6449d18
alexbowe <bowe.alexander@gmail.com>
2010-08-07 14:00:23 +1000

    pseudocode

--- keyphrase.py
+++ keyphrase.py
@@ -10,8 +10,14 @@ def mapper(key, value):
         fd.inc(word)
     yield key[0], fd.keys()[:3]
     
+# extract document id in mapper
+# mapper() -> stop-filtered stemmed words (or bi-words) + key, 1
+# sumreduce combiner
+# reducer() -> reads values into DistFreq and prints the top ten
+    
 def reducer(key,values):
-    yield key, values
+    for words in values:
+        yield key, values
 
 if __name__ == "__main__":
-    run(mapper, reducer)
+    run(mapper)
