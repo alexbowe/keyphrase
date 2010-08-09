e761b75a5718e85cd75c8458c1bf66ccdf38fdb2
alexbowe <bowe.alexander@gmail.com>
2010-08-10 00:24:49 +1000

    combiner functionality pushed to mapper

--- keyphrase.py
+++ keyphrase.py
@@ -9,29 +9,20 @@ separator = ','
 
 @opt("addpath", "yes")
 def mapper(key, value):
+    fd = FreqDist()
     for word in value.split():
         word = word.lower()
         if word not in stopwords:
             word = PorterStemmer().stem_word(word)
-            yield key[0], (word, 1)
-
-# Intermediate combiner... interested in knowing if there is a difference
-# when using this approach in the mapper instead... to avoid context switching
-# and sorting
-def combiner(key, values):
-    fd = FreqDist()
-    # Sum frequencies
-    for word, freq in values:
-        fd.inc(word, freq)
-    # Yield word/frequency tuples
-    for word, freq in fd.items():
-        yield key, (word, freq)
+            fd.inc(word, 1)
+    yield key[0], fd.items()
 
 def reducer(key, values):
     fd = FreqDist()
-    for word, freq in values:
-        fd.inc(word, freq)
+    for row in values:
+        for word, freq in row:
+            fd.inc(word, freq)
     yield key, separator.join(fd.keys()[:numKeyphrases])
 
 if __name__ == "__main__":
-    run(mapper, reducer, combiner)
+    run(mapper, reducer)
