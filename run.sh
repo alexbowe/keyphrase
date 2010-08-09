c3ae2b0eaef00f3d323564f9731cbd55acd29e7b
alexbowe <bowe.alexander@gmail.com>
2010-08-09 22:51:45 +1000

    run script has default file names now, as per the assignment spec, and runs the performance script at the end

--- run.sh
+++ run.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 DEPS=deps
-INPUT_DIR=$1
+INPUT_DIR=text #$1
+LOG=dumbo.log
+OUT=3134434.out #$2
 
-rm $2
+# Remove previous run if it exists
+rm $OUT
+
+# Run the MapReduce program
 dumbo start keyphrase.py \
     -libegg $DEPS/nltk-2.0b9-py2.6.egg \
     -libegg $DEPS/PyYAML.egg \
     -addpath yes \
     -input $INPUT_DIR/* \
-    -output $2 \
-    2> dumbo.log
-cat $2
-    
+    -output $OUT \
+    2> $LOG
+
+# Format for use with performance tester
+./format.pl $OUT
+
+# Assess the performance
+./performance.pl $OUT
