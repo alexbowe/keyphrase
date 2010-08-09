1ce89c78822a406e0fbd8101adf17131e92069ac
alexbowe <bowe.alexander@gmail.com>
2010-08-10 00:18:26 +1000

    add combiner and comments reminding me to test its functionality from the mapper, too

--- keyphrase.py
+++ keyphrase.py
@@ -15,10 +15,15 @@ def mapper(key, value):
             word = PorterStemmer().stem_word(word)
             yield key[0], (word, 1)
 
+# Intermediate combiner... interested in knowing if there is a difference
+# when using this approach in the mapper instead... to avoid context switching
+# and sorting
 def combiner(key, values):
     fd = FreqDist()
+    # Sum frequencies
     for word, freq in values:
         fd.inc(word, freq)
+    # Yield word/frequency tuples
     for word, freq in fd.items():
         yield key, (word, freq)
 
