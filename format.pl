15838a6f2efcacd96b8017cdad6eb2781ffe1dca
Cloudera Training <training@training-vm.(none)>
2010-08-18 01:28:17 -0700

    non-destructive version, and fixes issue with quotes being removed from key phrases

--- format.pl
+++ format.pl
@@ -1,8 +1,11 @@
-#! /usr/bin/perl -i -p
+#! /usr/bin/perl -p
 
-# Remove single quotes
-s/'//g;
+# Remove single quotes (when dumbo is run locally)
+s/^['"](.*)['"]\t+['"](.*)['"]$/\1\t\2/;
+
 # Replace tab with specified separator
 s/\t/ : /;
+
 # Extract file numbers
 s/.*\/([0-9]+)\.txt/\1/;
+
