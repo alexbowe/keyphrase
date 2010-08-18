cdc848f84da6870c3d7e4e655c2df25f8a85d8c1
Cloudera Training <training@training-vm.(none)>
2010-08-18 02:14:57 -0700

    comments and word tokenizing update

--- keyphrase.py
+++ keyphrase.py
@@ -1,6 +1,7 @@
 from dumbo import opt, run
 from nltk.stem.porter import PorterStemmer
 from nltk.probability import FreqDist
+from nltk import word_tokenize
 
 import mycorpus
 
@@ -8,17 +9,23 @@ stopwords = mycorpus.stopwords
 numKeyphrases = 10
 separator = ','
 
+# Takes (filename, line-num) pairs as keys, and a segment of text as the value
+# Outputs filenames as keys, and a list of (word, freq) pairs
+# Requires -addpath yes flag
 @opt("addpath", "yes")
 def mapper(key, value):
     fd = FreqDist()
-    for word in value.split():
+    for word in word_tokenize(value):
         word = word.lower()
-        # Strip unwanted symbols (quotes, %, etc...)
+        # TODO: Strip unwanted symbols (quotes, %, etc...)
+        # check length, and chunking
         if word not in stopwords:
             word = PorterStemmer().stem_word(word)
             fd.inc(word, 1)
     yield key[0], fd.items()
 
+# Takes filenames as keys, and a list of (word, freq) pairs
+# Outputs filename as key, and a comma separated string of top N words
 def reducer(key, values):
     fd = FreqDist()
     for row in values:
